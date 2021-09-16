cbd03cb608bc9318f0e0a211fc18dcfe2c0eb6a3
Hemangi3598 <90692254+Hemangi3598@users.noreply.github.com>
2021-09-16 16:41:30 +0530

    Add files via upload

---
+++ p5.py
@@ -0,0 +1,13 @@
+# wapp with using arithmetic operators
+a=3; b=4;
+print(a)
+print(b)
+print(a-b)
+print(a+b)
+print(a*b)
+print(a/b)
+print(a//b)
+print(a**b)
+print(a%b)
+# if we have to write a/b in round then we have to write print(round(a/b,2))
+# if we have to take nos from user then type a=float(input("enter a")); b=float(input("enter b"));
